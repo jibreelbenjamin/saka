212a541fbba0bb50796120da4f5e3e050e26ef24
El <sch.jibreelbenjamin@gmail.com>
2026-05-02 18:35:58 +0700

    downgrade versi preline v4.0.1, design layout sidebar menu dan toast

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" class="scroll-smooth relative h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/fav.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/fav.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/fav.png') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ $page ? ucfirst($page).' - ' : '' }}SAKA Sistem Transparansi Akademik</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+    </style>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
+    <script>
+        const html = document.querySelector('html');
+        const isLightOrAuto = localStorage.getItem('hs_theme') === 'light' || (localStorage.getItem('hs_theme') === 'auto' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+        const isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' || (localStorage.getItem('hs_theme') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+        const _serverTime = (function () {
+            const serverStart = new Date("{{ now()->format('Y-m-d H:i:s') }}").getTime();
+            const clientStart = Date.now();
+
+            function now() {
+                return new Date(serverStart + (Date.now() - clientStart));
+            }
+
+            return {
+                now
+            };
+        })();
+        const server_time = _serverTime.now()
+
+        if (isLightOrAuto && html.classList.contains('dark')) html.classList.remove('dark');
+        else if (isDarkOrAuto && html.classList.contains('light')) html.classList.remove('light');
+        else if (isDarkOrAuto && !html.classList.contains('dark')) html.classList.add('dark');
+        else if (isLightOrAuto && !html.classList.contains('light')) html.classList.add('light');
+
+        function updateClock() {
+            const el = document.getElementById('clock');
+            if (!el) return;
+
+            const options = {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            };
+
+            el.textContent = server_time.toLocaleTimeString('id-ID', options);
+            server_time.setSeconds(server_time.getSeconds() + 1);
+        }
+
+        setInterval(updateClock, 1000);
+    </script>
+</head>
+<body class="bg-gray-50 dark:bg-neutral-900 h-full">
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-head>
+    <div class="flex items-center justify-center min-h-screen py-8 px-4 mx-auto my-auto lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-blue-500">404</h1>
+            <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Halaman tidak ditemukan</p>
+            <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Maaf, kami tidak dapat menemukan halaman yang kamu cari. Coba periksa URL-nya atau kembali ke beranda.</p>
+            <a href="{{ env('APP_URL') }}" class="inline-flex text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4">Kembali ke beranda</a>
+        </div>   
+    </div>
+</x-head>
