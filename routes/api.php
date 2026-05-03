dce2b3e7f93b907b831b6a712db12c6c83070ad6
Jibreel Benjamin <sch.jibreelbenjamin@gmail.com>
2026-05-03 12:06:02 +0700

    Feature/api/kelas (#9)
    * Menambahkan response key total dan data pada traits di fungsi errorResponse() untuk mencegah error saat mengambil data null
    
    * Selesai kelas API

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\KelasController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    // Kelas
+    Route::get('/kelas', [KelasController::class, 'index']);
+    Route::post('/kelas', [KelasController::class, 'store']);
+    Route::get('/kelas/{id}', [KelasController::class, 'show']);
+    Route::put('/kelas/{id}', [KelasController::class, 'update']);
+    Route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
 });
