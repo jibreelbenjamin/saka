bf19ae01a747827bc1fa1c8bdf10fff23aa27378
AdhitamaAzar <adhitamagaming969@gmail.com>
2026-05-08 09:24:06 +0700

    Selesai controller api  guru

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\KelasController;
+use App\Http\Controllers\Api\GuruController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -16,4 +17,23 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/kelas/{id}', [KelasController::class, 'show']);
     Route::put('/kelas/{id}', [KelasController::class, 'update']);
     Route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
+
+     Route::put('/guru/update-password/{id}', [GuruController::class, 'updatePassword'])
+        ->name('guru.update-password');
+
+    Route::get('/guru', [GuruController::class, 'index'])
+        ->name('guru.index');
+
+    Route::post('/guru', [GuruController::class, 'store'])
+        ->name('guru.store');
+
+    Route::get('/guru/{id}', [GuruController::class, 'show'])
+        ->name('guru.show');
+
+    Route::put('/guru/{id}', [GuruController::class, 'update'])
+        ->name('guru.update');
+
+    Route::delete('/guru/{id}', [GuruController::class, 'destroy'])
+        ->name('guru.destroy');
+
 });
