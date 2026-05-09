65d93f3361fb30e6d7c0adacb80f2fa5dbfc2465
davidnfy <davidnafisy3@gmail.com>
2026-05-09 08:34:34 +0700

    add input

--- resources/views/dashboard/admin/form.blade.php
+++ resources/views/dashboard/admin/form.blade.php
@@ -28,21 +28,61 @@
             <div class="py-2 sm:py-4 px-2">
               <div class="p-4 space-y-5">
                 <!-- Grid -->
+                <!-- Username -->
                 <div class="grid sm:grid-cols-12 gap-y-1.5 sm:gap-y-0 sm:gap-x-5">
                   <div class="sm:col-span-3">
                     <label class="sm:mt-2.5 inline-block text-sm text-gray-500 dark:text-neutral-500">
-                        ...
+                        Username
                     </label>
                   </div>
 
                   <div class="sm:col-span-9">
-                    <input type="text" name="..." value="{{ old('...') }}" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600" placeholder="..." autocomplete="off">
+                    <input type="text" name="username" value="{{ old('username') }}" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600" placeholder="Masukkan username" autocomplete="off" required>
                   </div>
-
                 </div>
                 <!-- End Grid -->
 
-                ...
+                <!-- Nama -->
+                <div class="grid sm:grid-cols-12 gap-y-1.5 sm:gap-y-0 sm:gap-x-5">
+                  <div class="sm:col-span-3">
+                    <label class="sm:mt-2.5 inline-block text-sm text-gray-500 dark:text-neutral-500">
+                        Nama
+                    </label>
+                  </div>
+
+                  <div class="sm:col-span-9">
+                    <input type="text" name="nama" value="{{ old('nama') }}" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600" placeholder="Masukkan nama lengkap" autocomplete="off" required>
+                  </div>
+                </div>
+                <!-- End Grid -->
+
+                <!-- Password -->
+                <div class="grid sm:grid-cols-12 gap-y-1.5 sm:gap-y-0 sm:gap-x-5">
+                  <div class="sm:col-span-3">
+                    <label class="sm:mt-2.5 inline-block text-sm text-gray-500 dark:text-neutral-500">
+                        Password
+                    </label>
+                  </div>
+
+                  <div class="sm:col-span-9">
+                    <input type="password" name="password" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600" placeholder="Masukkan password" autocomplete="off" required>
+                  </div>
+                </div>
+                <!-- End Grid -->
+
+                <!-- Konfirmasi Password -->
+                <div class="grid sm:grid-cols-12 gap-y-1.5 sm:gap-y-0 sm:gap-x-5">
+                  <div class="sm:col-span-3">
+                    <label class="sm:mt-2.5 inline-block text-sm text-gray-500 dark:text-neutral-500">
+                        Konfirmasi Password
+                    </label>
+                  </div>
+
+                  <div class="sm:col-span-9">
+                    <input type="password" name="password_confirmation" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600" placeholder="Konfirmasi password" autocomplete="off" required>
+                  </div>
+                </div>
+                <!-- End Grid -->
 
               </div>
             </div>
