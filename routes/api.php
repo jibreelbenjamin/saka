23fedb09795b95b83d6d276ee6d4599520596325
AdhitamaAzar <adhitamagaming969@gmail.com>
2026-05-09 11:16:44 +0700

    rapi rapih routing

--- routes/api.php
+++ routes/api.php
@@ -18,23 +18,11 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('/kelas/{id}', [KelasController::class, 'update']);
     Route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
 
-     Route::put('/guru/update-password/{id}', [GuruController::class, 'updatePassword'])
-        ->name('guru.update-password');
-
-    Route::get('/guru', [GuruController::class, 'index'])
-        ->name('guru.index');
-
-    Route::post('/guru', [GuruController::class, 'store'])
-        ->name('guru.store');
-
-    Route::get('/guru/{id}', [GuruController::class, 'show'])
-        ->name('guru.show');
-
-    Route::put('/guru/{id}', [GuruController::class, 'update'])
-        ->name('guru.update');
-
-    Route::delete('/guru/{id}', [GuruController::class, 'destroy'])
-        ->name('guru.destroy');
-    Route::put('/guru/update-password/{id}', [GuruController::class, 'updatePassword'])
-    ->name('guru.update-password');
+    // Guru
+    Route::get('/guru', [GuruController::class, 'index']);
+    Route::post('/guru', [GuruController::class, 'store']);
+    Route::get('/guru/{id}', [GuruController::class, 'show']);
+    Route::put('/guru/{id}', [GuruController::class, 'update']);
+    Route::delete('/guru/{id}', [GuruController::class, 'destroy']);
+    Route::put('/guru/update-password/{id}', [GuruController::class, 'updatePassword']);
 });
