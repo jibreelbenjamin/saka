64bcd6dc3a98d9281d8a7e58f1aeb13ac6bda0e5
Jibreel Benjamin <sch.jibreelbenjamin@gmail.com>
2026-05-09 12:08:27 +0700

    Feature/api/guru (#18)
    * Selesai controller api  guru
    
    * menambahkan fitur update password dan membetulkan routing api
    
    * rapi rapih routing
    
    ---------
    
    Co-authored-by: AdhitamaAzar <adhitamagaming969@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\KelasController;
+use App\Http\Controllers\Api\GuruController;
 use App\Http\Controllers\Api\SiswaController;
 use App\Http\Controllers\Api\WaliSiswaController;
 use App\Http\Controllers\Api\AdminController;
@@ -20,6 +21,14 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('/kelas/{id}', [KelasController::class, 'update']);
     Route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
 
+    // Guru
+    Route::get('/guru', [GuruController::class, 'index']);
+    Route::post('/guru', [GuruController::class, 'store']);
+    Route::get('/guru/{id}', [GuruController::class, 'show']);
+    Route::put('/guru/{id}', [GuruController::class, 'update']);
+    Route::delete('/guru/{id}', [GuruController::class, 'destroy']);
+    Route::put('/guru/update-password/{id}', [GuruController::class, 'updatePassword']);
+  
     // Siswa
     Route::get('/siswa', [SiswaController::class, 'index']);
     Route::post('/siswa', [SiswaController::class, 'store']);
