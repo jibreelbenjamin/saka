e4ea6c9e024ae229014750d1439eb8b8bbf4f8d5
fadhlurthoriq <muhammadfthoriq@gmail.com>
2026-05-11 19:06:11 +0700

    deleting data wali siswa menu

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -262,16 +262,6 @@
                         </a>
                     </li>
                     <li>
-                        <a class="{{ ($page == 'wali-siswa') ? $classPageActive : $classPageInactive }}" href="{{ route('develop') }}">
-                        Data wali siswa
-                        <div class="ms-auto">
-                            <span class="ms-auto inline-flex items-center gap-1.5 py-px px-1.5 rounded-sm text-[10px] leading-4 font-medium bg-yellow-100 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-300">
-                                DEV
-                            </span>
-                        </div>
-                        </a>
-                    </li>
-                    <li>
                         <a class="{{ ($page == 'guru') ? $classPageActive : $classPageInactive }}" href="{{ route('develop') }}">
                         Data guru
                         <div class="ms-auto">
