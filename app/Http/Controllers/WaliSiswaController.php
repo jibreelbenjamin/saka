a520e46b9e9e873f9f49d6c1a1752744165a1528
fadhlurthoriq <muhammadfthoriq@gmail.com>
2026-05-13 09:31:10 +0700

    fixing alamat & kontak form

--- app/Http/Controllers/WaliSiswaController.php
+++ app/Http/Controllers/WaliSiswaController.php
@@ -17,8 +17,8 @@ class WaliSiswaController extends Controller
 
     protected $rules = [
         'nama_wali'   => 'required|string|max:255',
-        'kontak'      => 'required|string|digits_between:10,15',
-        'alamat'      => 'required|string|max:255',
+        'kontak'      => 'nullable|string|digits_between:10,15',
+        'alamat'      => 'nullable|string|max:255',
         'status_wali' => 'required|string|max:255'
     ];
 
@@ -26,9 +26,7 @@ class WaliSiswaController extends Controller
         'nama_wali.required'      => 'Nama wali wajib diisi',
         'nama_wali.string'        => 'Nama wali harus berupa teks',
         'nama_wali.max'           => 'Nama wali maksimal 255 karakter',
-        'kontak.required'         => 'Kontak wajib diisi',
         'kontak.digits_between'   => 'Kontak harus terdiri dari 10 sampai 15 digit',
-        'alamat.required'         => 'Alamat wajib diisi',
         'alamat.max'              => 'Alamat maksimal 255 karakter',
         'status_wali.required'    => 'Status wali wajib diisi',
         'status_wali.in'          => 'Status wali tidak valid'
--- resources/views/dashboard/wali-siswa/setting.blade.php
+++ resources/views/dashboard/wali-siswa/setting.blade.php
@@ -56,7 +56,7 @@
 
                                 <div class="sm:col-span-9">
                                     <input type="text" name="kontak" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600"
-                                    placeholder="Kontak Wali Siswa" value="{{ $data->kontak }}" autocomplete="off">
+                                    placeholder="Boleh dikosongi" value="{{ $data->kontak }}" autocomplete="off">
                                 </div>
                                 <!-- End Col -->
                             </div>
@@ -72,7 +72,7 @@
 
                                 <div class="sm:col-span-9">
                                     <input type="text" name="alamat" class="py-1.5 sm:py-2 px-3 block w-full border-gray-200 rounded-lg sm:text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:placeholder:text-white/60 dark:focus:ring-neutral-600"
-                                    placeholder="Alamat Wali Siswa" value="{{ $data->alamat }}" autocomplete="off">
+                                    placeholder="Boleh dikosongi" value="{{ $data->alamat }}" autocomplete="off">
                                 </div>
                                 <!-- End Col -->
                             </div>
