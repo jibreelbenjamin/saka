1012081815498ed0f74315b940e1783f768e8b40
David Nafisy <davidnafisy3@gmail.com>
2026-05-14 11:53:37 +0700

    Feature/web/guru (#31)
    * add controller & routes for guru
    
    * updated search keys & route guru
    
    * guru
    
    * updated search keys controller guru
    
    * fix form pw guru
    
    ---------
    
    Co-authored-by: fadhlurthoriq <muhammadfthoriq@gmail.com>
    Co-authored-by: Jibreel Benjamin <sch.jibreelbenjamin@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,14 @@ Route::middleware('auth')->group(function () {
     Route::put('/kelas/update/{id}', [KelasController::class, 'update'])->name('kelas.update.action');
     Route::delete('/kelas/delete/{id}', [KelasController::class, 'delete'])->name('kelas.delete.action');
 
+    // Guru
+    Route::get('/guru', [GuruController::class, 'index'])->name('guru');
+    Route::get('/guru/create', [GuruController::class, 'create'])->name('guru.create');
+    Route::get('/guru/{id}', [GuruController::class, 'setting'])->name('guru.setting');
+    Route::post('/guru/add', [GuruController::class, 'add'])->name('guru.create.action');
+    Route::put('/guru/update/{id}', [GuruController::class, 'update'])->name('guru.update.action');
+    Route::delete('/guru/delete/{id}', [GuruController::class, 'delete'])->name('guru.delete.action');
+  
     // Siswa
     Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
     Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
